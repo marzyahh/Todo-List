df49f146106b40a71bdfb43efcf7e5b10babae7b
marzyahh <marzyahhooseini74@gmail.com>
2021-03-30 11:04:45 +0430

    Add search and delete method

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const deleteItems = document.querySelector('.delete');
+const searching = document.querySelector('.search input');
 
+// Add new Todo
 const createHtmlTemplate = todo => {
 
     const html = `
@@ -27,3 +30,47 @@ addForm.addEventListener('submit', e => {
         addForm.reset();
     }
 });
+
+// Delete an Item
+list.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
+// search methods
+const searched = (term) => {
+    Array.from(list.children)
+        .filter((item) => { return item.textContent.toLowerCase().includes(term) })
+        .forEach(item => { item.classList.remove('filtered') });
+
+    Array.from(list.children)
+        .filter((item) => { return !item.textContent.toLowerCase().includes(term) })
+        .forEach(item => { item.classList.add('filtered') });
+
+};
+
+// keyup event
+searching.addEventListener('keyup', () => {
+    const term = searching.value.trim();
+    searched(term);
+});
+
+// const searched = (searchLetter) => {
+//     const filter = Array.from(list.children).filter((searchLetter) => {
+//         if (searchLetter == list.children) {
+//             list.children.style = "block";
+//             console.log(list.children)
+
+//         } else {
+//             list.children.style = "none"
+//         }
+//     }).map(filter => { alert(filter.value) })
+// };
+
+// searching.addEventListener('keyup', e => {
+//     e.preventDefault();
+
+//     const searchLetter = searching.search.value.trim();
+//     searched(searchLetter);
+// })
